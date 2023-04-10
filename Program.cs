6123c3be3f054afd57eaca236eadddcc98429203
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-10 13:57:29 +0500

    task3

--- Program.cs
+++ Program.cs
@@ -10,26 +10,50 @@ namespace Takrorla
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("Enter Number  : ");
-            int input = int.Parse(Console.ReadLine());
+        { 
+           
 
-            Console.Write("Sum : " + Sum(input));
+            Random random = new Random();
+            int[][] arr = new int[5][];          
 
+            for (int i = 0; i < arr.Length;  i++)
+            {
+                arr[i] = new int[5];
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    arr[i][j] = random.Next(0, 10);
+                    Console.Write(arr[i][j]+"  ");
+                }
+                Console.WriteLine();
+            }
+            int h = 0;
+            int g = 0;           
+
+            Console.WriteLine($"Diaganal : {h=(diaganal(arr))}");
+            Console.WriteLine($"Oxirgi satr : {g=(ustun(arr))}");
+            bool result = true;
+            if (h < g ) result = false;
+            Console.WriteLine($"natija : {result}"); 
             Console.ReadLine();
-
         }
-        public static int Sum(int a)
+        public static int diaganal(int[][] arr)
         {
             int sum = 0;
-
-            while (a > 0)
-            {
-                sum += a % 10;
-                a /= 10;
+            for (int i = 0; i < arr.Length; i++)
+            {             
+                        sum += arr[i][i];                
             }
-
             return sum;
         }
+        public static int ustun(int[][] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {            
+                        sum += arr[4][i];                
+            }
+            return sum;
+        }
+        
     }
 }
